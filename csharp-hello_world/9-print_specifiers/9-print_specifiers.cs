c8bd21c622449a61b552d41bc8823cfe70ebe678
N4yth <nathan.dupuis31@gmail.com>
2025-12-02 09:07:50 +0100

    fix: correct currency value and improve formatting

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -5,8 +5,8 @@ class Program
 	static void Main(string[] args)
 	{
 		double percent = .7553;
-		double currency = 98765.4321;
+		double currency = -98765.4321;
         Console.WriteLine($"Percent: {percent.ToString("P")}");
-        Console.WriteLine($"Currency: ${currency.ToString("###,##0.00")}");
+        Console.WriteLine($"Currency: {currency.ToString("$###,##0.00")}");
 	}
 }
