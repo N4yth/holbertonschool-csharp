f250cf1bf11035aa5cb8c9c1d76faad86c56c37e
N4yth <nathan.dupuis31@gmail.com>
2025-12-02 11:40:49 +0100

    feat: implement PrintDiagonal method to print a diagonal line based on specified length

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+        {
+            for (int i = 0 ; i < length ; i++)
+            {
+                for (int Y = 0 ; Y < i ; Y++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\"); 
+            }
+        }
+        Console.WriteLine("");
+    }
+}
