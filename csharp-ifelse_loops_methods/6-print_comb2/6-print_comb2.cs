200a32ca3a151ec7150e3720b7b9c6ca0b3735cc
N4yth <nathan.dupuis31@gmail.com>
2025-12-02 10:50:44 +0100

    feat: add C# program to print combinations of two digits

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string separator = ", ";
+        for (int i = 0 ; i <= 9 ; i++)
+        {
+            for (int y = i + 1 ; y <= 9 ; y++)
+            {
+                if (i != 8)
+                {
+                    Console.Write($"{i}{y}{separator}");
+                }
+                else
+                {
+                    Console.Write($"{i}{y}\n");
+                }     
+            }
+        }
+    }
+}
