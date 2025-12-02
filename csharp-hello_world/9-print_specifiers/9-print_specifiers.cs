ff82cfa37cd850b532cc011e9099282f5dd73c73
N4yth <nathan.dupuis31@gmail.com>
2025-12-02 08:40:33 +0100

    fix : change the way to display percent

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-        Console.WriteLine($"Percent: {percent:0.00%}");
+        Console.WriteLine($"Percent: {percent*100}%");
         Console.WriteLine($"Currency: ${currency.ToString("###,##0.00")}");
 	}
 }
