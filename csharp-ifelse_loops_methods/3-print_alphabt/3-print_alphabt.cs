e36f0ec5c4c0a56c2c55159b58074c0e92f2b467
N4yth <nathan.dupuis31@gmail.com>
2025-12-02 10:34:19 +0100

    fix: correct logic in alphabet printing to exclude 'e' and 'q'

--- csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -6,7 +6,7 @@ class Program
     {
         for (int i = 97 ; i <= 122 ; i++)
         {
-            if (i != 101 || i != 113)
+            if (i != 101 && i != 113)
             {
                 Console.Write((char)i);  
             }   
