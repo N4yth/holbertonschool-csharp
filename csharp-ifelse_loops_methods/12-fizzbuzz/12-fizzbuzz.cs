fc6c03d6b2eb1c26371a1ff9721890b4e7a6d514
N4yth <nathan.dupuis31@gmail.com>
2025-12-02 11:47:31 +0100

    feat: implement FizzBuzz program to print numbers 1 to 100 with Fizz, Buzz, and FizzBuzz substitutions

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1 ; i < 101 ; i++)
+        {
+            if (i % 3 == 0)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write("FizzBuzz");
+                }
+                else{
+                    Console.Write("Fizz");
+                }       
+            }
+            else if (i % 5 == 0)
+            {
+                Console.Write("Buzz");
+            }
+            else
+            {
+                Console.Write(i);
+            }
+            Console.Write(" ");
+        }
+        Console.WriteLine("");
+    }
+}
