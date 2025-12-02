d3278d4ad3f82a1a50b509781eb73be34e65fe00
N4yth <nathan.dupuis31@gmail.com>
2025-12-02 10:43:53 +0100

    feat: add C# program to print numbers from 00 to 99 with a separator

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string separator = ", ";
+        for (int i = 0 ; i <= 99 ; i++)
+        {
+            if (i < 99)
+            {
+                Console.Write($"{i.ToString("00")}{separator}");
+            }
+            else
+            {
+                Console.Write($"{i.ToString("00")}\n");
+            }
+        }
+    }
+}
