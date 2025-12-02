5ea152e6d7c0c92c967df51c2e153b1594e75951
N4yth <nathan.dupuis31@gmail.com>
2025-12-02 10:28:52 +0100

    feat: add C# program and project file to print the alphabet

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 97 ; i <= 122 ; i++)
+        {
+            Console.Write((char)i);
+        }
+    }
+}
