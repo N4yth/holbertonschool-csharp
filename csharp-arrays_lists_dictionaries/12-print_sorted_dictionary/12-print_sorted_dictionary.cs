dd4fb4ffd31ff0434cd15d2ee749f9f134e89a2a
N4yth <nathan.dupuis31@gmail.com>
2025-12-03 10:28:01 +0100

    feat: implement PrintSorted method to display sorted dictionary entries

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        Dictionary<string, string> newDict = new Dictionary<string, string>();
+
+        newDict = myDict;
+        foreach (KeyValuePair<string, string> Entry in myDict.OrderBy(x => x.Key))
+        {
+            Console.WriteLine($"{Entry.Key}: {Entry.Value}");
+        }
+    }
+}
