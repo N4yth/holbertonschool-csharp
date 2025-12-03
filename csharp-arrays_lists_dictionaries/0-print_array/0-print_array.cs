951d95bdf14b8aae29d27b1733f931942e12c6eb
N4yth <nathan.dupuis31@gmail.com>
2025-12-03 08:39:30 +0100

    feat: implement CreatePrint method to generate and print an array of integers

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size >= 0)
+        {
+            int[] result = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+
+                result[i] = i;
+                Console.Write(i);
+                if (i != size)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine("");
+            return result;
+        }
+        else
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+    }
+}
