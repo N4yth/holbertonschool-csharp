e2cb7f61cd501daa5dd938b71ab5c10f677dd74a
N4yth <nathan.dupuis31@gmail.com>
2025-12-03 09:46:20 +0100

    feat: implement NumberOfKeys method to count the number of keys in a dictionary

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int size;
+        foreach (var ele in myDict){
+            size++;
+        }
+        return size;
+    }
+}
