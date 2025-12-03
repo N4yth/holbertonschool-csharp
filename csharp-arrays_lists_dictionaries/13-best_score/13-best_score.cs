c29d2754eeeeb2fe0830f275562abacc638e1e35
N4yth <nathan.dupuis31@gmail.com>
2025-12-03 10:46:04 +0100

    feat: implement BestScore method to determine the highest score from a dictionary

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if(myList.Count == 0)
+        {
+            return "None";
+        }
+        string winner = "";
+        int bscore = 0;
+        foreach (KeyValuePair<string, int> Entry in myList)
+        {
+            if (bscore < Entry.Value)
+            {
+                winner = Entry.Key;
+                bscore = Entry.Value;
+            }
+        }
+        return winner;
+    }
+}
