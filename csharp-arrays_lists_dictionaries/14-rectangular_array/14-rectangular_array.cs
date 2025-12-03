642b8209db1aaa3f586a843fe130616057fc5a20
N4yth <nathan.dupuis31@gmail.com>
2025-12-03 10:52:04 +0100

    feat: add rectangular array C# console application

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0 ; i < 5 ; i++)
+        {
+            for (int y = 0 ; y < 5 ; y++)
+            {
+                if (i == 2 && y == 2)
+                {
+                    Console.Write("1");
+                }
+                else
+                {
+                    Console.Write("0");
+                }
+                if (y < 4)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
