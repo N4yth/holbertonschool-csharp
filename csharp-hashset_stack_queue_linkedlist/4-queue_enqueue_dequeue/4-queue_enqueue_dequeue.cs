8e70f163c179eaf6d2bb8e89ebc19c7df10b1587
N4yth <nathan.dupuis31@gmail.com>
2025-12-08 11:36:59 +0100

    feat : make a C# programme that manipulate a Queue

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        string remove = "";
+        int nbele = 0;
+        foreach (var item in aQueue)
+        {
+            nbele++;
+        }
+        Console.WriteLine($"Number of items: {nbele}");
+        if (nbele == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine($"First item: {aQueue.Dequeue()}");
+        aQueue.Enqueue(newItem);
+        Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
+        if (aQueue.Contains(search))
+        {
+            while (remove != search)
+            {
+                remove = aQueue.Dequeue();
+            }
+        }
+        return aQueue;
+    }
+}
