22c310f0e1bd5eb9a9c445f79ad4d9637efd42ff
N4yth <nathan.dupuis31@gmail.com>
2025-12-08 10:45:00 +0100

    feat: add DifferentElements method and main program to find unique elements in two lists

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>(list1.Count + list2.Count);
+        foreach (int i in list1)
+            if (!list2.Contains(i))
+            {
+                newList.Add(i);
+            }
+        foreach (int i in list2)
+            if (!list1.Contains(i))
+            {
+                newList.Add(i);
+            }
+        newList.Sort();
+        return newList;
+    }
+}
