fefc0ee0fb3e46234f4849fa002b123809587b81
N4yth <nathan.dupuis31@gmail.com>
2025-12-08 13:25:27 +0100

    feat : make a C# function that create a new node at a correct order in a linkedlist

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current;
+
+        current = myLList.First;
+        while (current != null)
+        {
+            if (current.Value > n)
+            {
+                myLList.AddBefore(current, n);
+                break;
+            }
+            current = current.Next;
+        }
+        return current;
+    }
+}
