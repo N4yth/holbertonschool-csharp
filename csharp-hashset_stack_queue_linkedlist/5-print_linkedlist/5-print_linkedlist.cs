d3c83248373febce395855450d7e68f5263f02dc
N4yth <nathan.dupuis31@gmail.com>
2025-12-08 11:46:11 +0100

    feat : create a linledlist of a given size

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newLL = new LinkedList<int>();
+
+        for (int i = 0 ; i < size ; i++)
+        {
+            Console.WriteLine($"{i}");
+            newLL.AddLast(i);
+        }
+        return newLL;
+    }
+}
