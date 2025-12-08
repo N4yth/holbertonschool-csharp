974e5646ee3f3704722155f4f281c0c2d6dd4e84
N4yth <nathan.dupuis31@gmail.com>
2025-12-08 09:53:50 +0100

    feat: implement CommonElements method and main program for finding common integers in two lists

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+        foreach (int i in list2)
+            if (list1.Contains(i))
+            {
+                newList.Add(i);
+            }
+        return newList;
+    }
+}
