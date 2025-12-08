dbb8ca59ef7e150ccda09c43f0fa3f009b2bf932
N4yth <nathan.dupuis31@gmail.com>
2025-12-08 13:31:49 +0100

    fix : handle edge case

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -10,9 +10,14 @@ class LList
         current = myLList.First;
         while (current != null)
         {
-            if (current.Value > n)
+            if ((current.Value > n && current.Next != null) || (current.Value > n && current.Next == null))
             {
                 myLList.AddBefore(current, n);
+                break;
+            }
+            else if (current.Value < n && current.Next == null)
+            {
+                myLList.AddAfter(current, n);
                 break;
             }
             current = current.Next;
