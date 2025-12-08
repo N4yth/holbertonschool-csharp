25b359e96372a3dd9fea6e0b9826df07ae1578f1
N4yth <nathan.dupuis31@gmail.com>
2025-12-08 09:45:03 +0100

    feat: add unique sum functionality with example usage

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // Main - entry point
+    public static int Sum(List<int> myList)
+    {
+        int result = 0;
+        HashSet<int> hash = new HashSet<int>();
+        foreach (var item in myList)
+        {
+            hash.Add(item);
+        }
+        foreach (var item in hash)
+        {
+            result += item;
+        }
+        return result;
+    }
+}
