8fcde7029887f0afba6daa1c11143f6f9989d972
N4yth <nathan.dupuis31@gmail.com>
2025-12-08 13:43:43 +0100

    feat : create function that remove a node at a given index

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current;
+        int idx = 0;
+        current = myLList.First;
+        while (current != null)
+        {
+            if (index == idx)
+            {
+                myLList.Remove(current);
+                break;
+            }
+            idx++;
+            current = current.Next;
+        }
+    }
+}
