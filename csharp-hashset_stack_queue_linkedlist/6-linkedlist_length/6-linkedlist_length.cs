e2e52e2b5bfa5d87c005df4df18487491fa98db5
N4yth <nathan.dupuis31@gmail.com>
2025-12-08 12:21:55 +0100

    feat : make a C# script that return the number of element of an linkedlist

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int nbele = 0;
+        foreach(var i in myLList) 
+            nbele++;
+        return nbele;
+    }
+}
