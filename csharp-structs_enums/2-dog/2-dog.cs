ff7494d5ba3d31daf96e229a50a9a83ad29d4120
N4yth <nathan.dupuis31@gmail.com>
2026-01-05 11:39:17 +0100

    feat: add Dog struct and main program with Rating enum

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,22 @@
+﻿enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string dog_name, float dog_age, string dog_owner, Rating dog_rating)
+    {
+        name = dog_name;
+        age = dog_age;
+        owner = dog_owner;
+        rating = dog_rating;
+    }
+}
