6c8226a9b2b9e3f297002a5c595e1cfa66636e4b
N4yth <nathan.dupuis31@gmail.com>
2026-01-05 11:42:55 +0100

    feat: add Dog struct, Rating enum, and main program to demonstrate usage

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,26 @@
+﻿enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string dog_name, float dog_age, string dog_owner, Rating dog_rating)
+    {
+        name = dog_name;
+        age = dog_age;
+        owner = dog_owner;
+        rating = dog_rating;
+    }
+    public override string ToString()
+    {
+        return $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+    }
+}
