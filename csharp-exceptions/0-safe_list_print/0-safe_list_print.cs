e1dcfc980d4268227a564d195d07cc26cf6b29b8
N4yth <nathan.dupuis31@gmail.com>
2026-01-05 10:27:45 +0100

    feat : add SafePrint method to handle safe printing of list elements

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int printed = 0;
+        try
+        {
+            for (int i = 0 ; i < n ; i++)
+            {
+                Console.WriteLine($"{myList[i]}");
+                printed += 1;
+            }
+        }
+        catch
+        {
+            
+        }
+        return printed;
+    }
+}
