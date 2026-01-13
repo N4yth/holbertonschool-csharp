b236b765196e7dde7bbb1bae2d11b9d46559937d
N4yth <nathan.dupuis31@gmail.com>
2026-01-13 09:45:36 +0100

    feat: add Zombie class and main program to demonstrate instantiation

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This is a class that represent zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represent the health of a zombie.
+        /// </summary>
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
