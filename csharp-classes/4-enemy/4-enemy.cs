406426baec60f7014dc3cf4b52e7826327a31811
N4yth <nathan.dupuis31@gmail.com>
2026-01-13 10:11:09 +0100

    feat: implement Zombie class with health management and main program for demonstration

---
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This is a class that represent zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represent the health of a zombie.
+        /// </summary>
+        private int health;
+
+        /// <summary>
+        /// Represent the name of a zombie.
+        /// </summary>
+        private string name = "(No name)";
+
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
