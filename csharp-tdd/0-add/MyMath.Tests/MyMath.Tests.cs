93701f8f99656264ccce60c0c39663947d5cc072
N4yth <nathan.dupuis31@gmail.com>
2026-01-13 14:49:02 +0100

    fix: correct spelling in test method names and add additional test cases for zero inputs

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using NUnit.Framework;
 using MyMath;
 
 public class Tests
@@ -18,9 +19,23 @@ public class Tests
     }
 
     [Test]
-    public void Add_pisitifAndNegatif()
+    public void Add_PositifAndNegatif()
     {
         var result = Operations.Add(-1, 2);
         Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Add_TwoZero()
+    {
+        var result = Operations.Add(0, 0);
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Add_OneZero()
+    {
+        var result = Operations.Add(0, 2);
+        Assert.That(result, Is.EqualTo(2));
     }
 }
