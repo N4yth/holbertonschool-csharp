c79ed2e10734883be3d2eac7303b3da5e78c50d7
N4yth <nathan.dupuis31@gmail.com>
2026-01-13 09:56:29 +0100

    feat: add Zombie class and main program to demonstrate instantiation and health validation

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This is a class that represent zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represent the health of a zombie.
+        /// </summary>
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
