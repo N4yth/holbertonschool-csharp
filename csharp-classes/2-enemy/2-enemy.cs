bdf12879681fe714820909db8022b644013a2e60
N4yth <nathan.dupuis31@gmail.com>
2026-01-13 09:57:33 +0100

    fix: ensure health is set correctly in Zombie constructor

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -24,6 +24,7 @@ namespace Enemies
             {
                 throw new ArgumentException("Health must be greater than or equal to 0");
             }
+            health = value;
         }
     }
 }
