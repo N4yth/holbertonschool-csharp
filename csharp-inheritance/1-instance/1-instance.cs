b981d47558440d2c58eb6dc00a6669277c144b0e
N4yth <nathan.dupuis31@gmail.com>
2026-01-19 14:30:17 +0100

    feat: add Obj class with IsInstanceOfArray method and main program for type checking

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType().Equals(typeof(Array)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
