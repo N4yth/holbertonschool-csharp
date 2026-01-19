f45f8aedc6c49fa2b168a8cd683aed293b950667
N4yth <nathan.dupuis31@gmail.com>
2026-01-19 14:26:00 +0100

    feat: implement Obj class with IsOfTypeInt method and create main program for type checking

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType().Equals(typeof(int)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
