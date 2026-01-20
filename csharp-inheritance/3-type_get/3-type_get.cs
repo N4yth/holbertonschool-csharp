4c93d935a0f0c379cc359216b5d1b4a19ab8e4bc
N4yth <nathan.dupuis31@gmail.com>
2026-01-20 10:39:02 +0100

    feat: add Obj class with Print method and main program for type and property listing

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        Type type = myObj.GetType();
+
+        foreach (PropertyInfo property in type.GetProperties())
+        {
+            Console.WriteLine(property.Name);
+        }
+        
+        foreach (MethodInfo method in type.GetMethods())
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
