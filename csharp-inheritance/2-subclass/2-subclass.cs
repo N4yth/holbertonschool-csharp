9d7751203fb1ade4f896501d699cbd03f0998c42
N4yth <nathan.dupuis31@gmail.com>
2026-01-20 10:19:55 +0100

    feat: add Obj class with IsOnlyASubclass method and main program for subclass checking

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
