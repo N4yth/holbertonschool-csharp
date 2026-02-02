1d1f8cc96f900164c523f925d04c93ae2b654094
N4yth <nathan.dupuis31@gmail.com>
2026-02-02 17:28:14 +0100

    feat : create a function that calculate the value of a 3D or 2D vector

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Count() == 2)
+        {
+            int a = vector[0] * vector[0];
+            int b = vector[1] * vector[1];
+            return Math.Sqrt(a + b);
+        }
+        else if (vector.Count() == 3)
+        {
+            int a = vector[0] * vector[0];
+            int b = vector[1] * vector[1];
+            int c = vector[2] * vector[2];
+            return Math.Sqrt(a + b + c);;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+}
