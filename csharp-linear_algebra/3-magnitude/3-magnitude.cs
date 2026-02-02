165c653bed17131b658cb1afa6fde57ac6238b70
N4yth <nathan.dupuis31@gmail.com>
2026-02-02 17:31:35 +0100

    fix : use double instead of int for field

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -5,17 +5,17 @@ class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
-        if (vector.Count() == 2)
+        if (vector.Count == 2)
         {
-            int a = vector[0] * vector[0];
-            int b = vector[1] * vector[1];
+            double a = vector[0] * vector[0];
+            double b = vector[1] * vector[1];
             return Math.Sqrt(a + b);
         }
-        else if (vector.Count() == 3)
+        else if (vector.Count == 3)
         {
-            int a = vector[0] * vector[0];
-            int b = vector[1] * vector[1];
-            int c = vector[2] * vector[2];
+            double a = vector[0] * vector[0];
+            double b = vector[1] * vector[1];
+            double c = vector[2] * vector[2];
             return Math.Sqrt(a + b + c);;
         }
         else
