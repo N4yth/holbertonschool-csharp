8127382d7e5d9a3a15d41f64e35d0f9b5f98b205
N4yth <nathan.dupuis31@gmail.com>
2026-02-02 18:03:15 +0100

    feat : create a function that do the sum of two vector

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Count() == 2 && vector2.Count() == 2)
+        {
+            return [vector1[0] + vector2[0], vector1[1] + vector2[1]];
+        }
+        else if (vector1.Count() == 3 && vector2.Count() == 3)
+        {
+            return [vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]];
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
