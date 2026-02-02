f0757f8b467c3b8b3936333f756c7d22856df999
N4yth <nathan.dupuis31@gmail.com>
2026-02-02 18:05:59 +0100

    fix : create result variable

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -8,11 +8,13 @@ class VectorMath
     {
         if (vector1.Count() == 2 && vector2.Count() == 2)
         {
-            return [vector1[0] + vector2[0], vector1[1] + vector2[1]];
+            double[] result = {vector1[0] + vector2[0], vector1[1] + vector2[1]};
+            return result;
         }
         else if (vector1.Count() == 3 && vector2.Count() == 3)
         {
-            return [vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]];
+            double[] result = {vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]};
+            return result;
         }
         else
         {
