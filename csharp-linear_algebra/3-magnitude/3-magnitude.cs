3d8586fc20110c070863fb95cb7adca54a01963e
N4yth <nathan.dupuis31@gmail.com>
2026-02-02 17:38:51 +0100

    feat : round the result to be less long

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -10,14 +10,14 @@ class VectorMath
         {
             double a = vector[0] * vector[0];
             double b = vector[1] * vector[1];
-            return Math.Sqrt(a + b);
+            return Math.Round(Math.Sqrt(a + b), 2);
         }
         else if (vector.Count() == 3)
         {
             double a = vector[0] * vector[0];
             double b = vector[1] * vector[1];
             double c = vector[2] * vector[2];
-            return Math.Sqrt(a + b + c);;
+            return Math.Round(Math.Sqrt(a + b + c), 2);
         }
         else
         {
