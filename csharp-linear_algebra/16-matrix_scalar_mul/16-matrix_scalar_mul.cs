b985a7e8812e207a59e42098765dc552b2e31973
N4yth <nathan.dupuis31@gmail.com>
2026-02-03 15:28:49 +0100

    feat : create a function that multiplies a matrix and a scalar and returns the resulting matrix

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.Length == 2 || matrix.Length == 3)
+        {
+            double[,] result = new double[matrix.Length, matrix.Length];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    result[i,j] = matrix[i, j] * scalar;
+                }
+            }
+            return result;
+        }
+        else
+        {
+            double[,] result = {{-1}};
+            return result;
+        }
+    }
+}
