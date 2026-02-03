5bea7def93cfdf0e51a2eb6de06f5e29794b4f7d
N4yth <nathan.dupuis31@gmail.com>
2026-02-03 15:23:09 +0100

    fix : add element to the new matrix with coord

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -7,19 +7,19 @@ class MatrixMath
     {
         if (matrix1.Length == matrix2.Length && (matrix1.Length == 4 || matrix1.Length == 9))
         {
-            double[,] result;
+            double[,] result = new double[matrix1.Length, matrix1.Length];
             for (int i = 0; i < matrix1.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix1.GetLength(1); j++)
                 {
-                    result.Add(matrix1[i, j] + matrix2[i, j]);
+                    result[i,j] = matrix1[i, j] + matrix2[i, j];
                 }
             }
             return result;
         }
         else
         {
-            double[,] result = {-1};
+            double[,] result = {{-1}};
             return result;
         }
     }
