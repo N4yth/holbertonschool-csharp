33cf3001ca396ffcd6e62fbaee1e2b6f20573f3b
N4yth <nathan.dupuis31@gmail.com>
2026-02-03 14:46:18 +0100

    feat : make a function that do the dot product of 2 vector

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Count() == 2 && vector2.Count() == 2)
+        {
+            return Math.Round((vector1[0] * vector2[0]) + (vector1[1] * vector2[1]), 2);
+        }
+        else if (vector1.Count() == 3 && vector2.Count() == 3)
+        {
+            return Math.Round((vector1[0] * vector2[0]) + (vector1[1] * vector2[1]) + (vector1[2] * vector2[2]), 2);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
