840c223513f6c9561b7e9e02132bf77f06aa3ac3
N4yth <nathan.dupuis31@gmail.com>
2026-02-03 15:06:35 +0100

    feat : create a function that sum 2 matric 2d or 3d

---
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.Length == matrix2.Length && (matrix1.Length == 4 || matrix1.Length == 9))
+        {
+            double[,] result;
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    result.Add(matrix1[i, j] + matrix2[i, j]);
+                }
+            }
+            return result;
+        }
+        else
+        {
+            return [-1];
+        }
+    }
+}
