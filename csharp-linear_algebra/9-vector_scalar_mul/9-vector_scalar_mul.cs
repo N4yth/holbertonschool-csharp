a44103d7aa5d53ec9a703864b00c336f6ed17fb3
N4yth <nathan.dupuis31@gmail.com>
2026-02-03 14:23:51 +0100

    feat : create a function that multiply vector and scalar

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Count() == 2 || vector.Count() == 3)
+        {
+            if (vector.Count() == 2)
+            {
+                double[] result = {vector[0] * scalar, vector[1] * scalar};
+            }
+            else
+            {
+                double[] result = {vector[0] * scalar, vector[1] * scalar, vector[2] * scalar};
+            }
+            return result;
+        }
+        else
+        {
+            double[] result = {-1};
+            return result;
+        }
+    }
+}
