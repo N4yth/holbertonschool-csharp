1b7ab74e2c3eaff492f3c99c66612f744412d872
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 14:50:39 +0100

    feat :

---
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using System.Threading.Tasks.Dataflow;
+using System.Xml.XPath;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        if (matrix.GetLength(1) != 2 || matrix.GetLength(1) != 3 && (matrix.GetLength(0) == matrix.GetLength(1)))
+        {
+            return new double[,] {-1};
+        }
+        double result;
+        if (matrix.GetLength(1) == 2)
+        {
+            result = matrix[0, 0]*matrix[1, 1] - matrix[0, 1]*matrix[1, 0];
+        }
+        else
+        {
+            return matrix[0,0]*matrix[1,1]*matrix[2,2]
+                + matrix[0,1]*matrix[1,2]*matrix[2,0]
+                + matrix[0,2]*matrix[1,0]*matrix[2,1]
+                - matrix[0,2]*matrix[1,1]*matrix[2,0]
+                - matrix[0,1]*matrix[1,0]*matrix[2,2]
+                - matrix[0,0]*matrix[1,2]*matrix[2,1];
+        }
+        return result;
+    }
+}
