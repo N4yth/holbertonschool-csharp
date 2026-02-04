8aeb48261d25afca6a5980d41630f811e19d5e1a
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 13:43:07 +0100

    feat : create a methode to shear a matrix

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        if (direction != "y" || direction != "x" || matrix.GetLength(1) == matrix.GetLength(0))
+        {
+            return new double[,] { { -1 } };
+        }
+        double[,] result = new double[rows, Rcol];
+
+        if (direction == "y")
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                result[i, 0] = matrix[i, 0] + matrix[0, i] * factor;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                result[0, i] = matrix[0, i] + matrix[i, 0] * factor;
+            }
+        }
+        return result;
+    }
+}
