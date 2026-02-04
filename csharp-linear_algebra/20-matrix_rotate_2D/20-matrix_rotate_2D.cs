16b562d6c4054153b149d2f76a41f77dfc2e57e9
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 09:56:46 +0100

    feat : create a methode to rotate 2x2 matrix

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        int row = matrix.GetLength(0);
+        int col = matrix.GetLength(1);
+        double[,] rotation = new double[,] {{0, -1}, {1, 0}};
+        int Rcol = rotation.GetLength(1);
+        if (row != 2)
+        {
+            double[,] result = {{-1}};
+            return result;
+        }
+        else
+        {
+            double[,] result = new double[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < Rcol; j++)
+                {
+                    double temp = 0;
+                    for (int k = 0; k < col; k++)
+                    {
+                        temp += matrix[i, k] * rotation[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            }
+            return result;
+        }
+    }
+}
