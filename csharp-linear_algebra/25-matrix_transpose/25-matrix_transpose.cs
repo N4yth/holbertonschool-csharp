541b87f80e4d6f8f2984b8b14d5d614988728c3e
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 14:20:52 +0100

    feat : create methode that transpose a matrix

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using System.Threading.Tasks.Dataflow;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        if (matrix.GetLength(1) !=  matrix.GetLength(0))
+        {
+            return new double[,] { { -1 } };
+        }
+        double[,] result = new double[ matrix.GetLength(0),  matrix.GetLength(1)];
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                result[i, j] = matrix[j, i];
+            }
+        }
+        return result;
+    }
+}
