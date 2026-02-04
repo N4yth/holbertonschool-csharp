11b5b91f972d26f8256bd2947f37bc1bf484b249
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 15:43:28 +0100

    fix

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class MatrixMath
 {
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        if (vector1.Length() != 3 || vector2.Length() != 3)
+        if (vector1.Count() != 3 || vector2.Count() != 3)
         {
-            return new double[] {{-1}};
+            double[] result = {{-1}};
+            return result;
         }
         return new double[] {{
             vector1[2]*vector2[3] - vector1[3]*vector2[2],
