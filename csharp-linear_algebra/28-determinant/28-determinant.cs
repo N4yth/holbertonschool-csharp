9d6b94123cc4f8aba6224fc846b1467b7317a86e
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 15:01:16 +0100

    fix

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -14,16 +14,16 @@ class MatrixMath
         }
         if (matrix.GetLength(1) == 2)
         {
-            return matrix[0, 0]*matrix[1, 1] - matrix[0, 1]*matrix[1, 0];
+            return Math.Round(matrix[0, 0]*matrix[1, 1] - matrix[0, 1]*matrix[1, 0], 2);
         }
         else
         {
-            return matrix[0,0]*matrix[1,1]*matrix[2,2]
+            return Math.Round(matrix[0,0]*matrix[1,1]*matrix[2,2]
                 + matrix[0,1]*matrix[1,2]*matrix[2,0]
                 + matrix[0,2]*matrix[1,0]*matrix[2,1]
                 - matrix[0,2]*matrix[1,1]*matrix[2,0]
                 - matrix[0,1]*matrix[1,0]*matrix[2,2]
-                - matrix[0,0]*matrix[1,2]*matrix[2,1];
+                - matrix[0,0]*matrix[1,2]*matrix[2,1], 2);
         }
     }
 }
