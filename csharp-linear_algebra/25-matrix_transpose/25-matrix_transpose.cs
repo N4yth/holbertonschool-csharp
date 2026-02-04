e30684d8cd2db9be3b0bf66ee9e62dd21f9e8307
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 14:26:52 +0100

    fix return result

--- csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -9,7 +9,7 @@ class MatrixMath
     {
         if (matrix.GetLength(1) ==  0)
         {
-            return new double[,] { {} };
+            return new double[,] {};
         }
         double[,] result = new double[ matrix.GetLength(1),  matrix.GetLength(0)];
         for (int i = 0; i < matrix.GetLength(1); i++)
