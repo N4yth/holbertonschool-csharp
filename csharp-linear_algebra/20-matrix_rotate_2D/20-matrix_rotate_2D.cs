e371996e62cf0345ea60e4ccbb2bff59c4db240a
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 10:16:41 +0100

    fix : round value for rotation matrix

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -7,7 +7,7 @@ class MatrixMath
     {
         int row = matrix.GetLength(0);
         int col = matrix.GetLength(1);
-        double[,] rotation = new double[,] {{Math.Cos(angle), -Math.Sin(angle)}, {Math.Sin(angle), Math.Cos(angle)}};
+        double[,] rotation = new double[,] {{Math.Round(Math.Cos(angle)), Math.Round(-Math.Sin(angle))}, {Math.Round(Math.Sin(angle)), Math.Round(Math.Cos(angle))}};
         int Rcol = rotation.GetLength(1);
         if (row != 2)
         {
