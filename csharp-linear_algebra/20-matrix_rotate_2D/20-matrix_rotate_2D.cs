b6ed3e1b1a4fc0933baa3392aa3eec400c521e0c
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 10:31:43 +0100

    fix

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -11,17 +11,23 @@ class MatrixMath
         {
             return new double[,] { { -1 } };
         }
-        double Tangle = angle * Math.PI / 180.0;
         double[,] rotation = new double[,] {
-            { Math.Cos(Tangle), -Math.Sin(Tangle) },
-            { Math.Sin(Tangle),  Math.Cos(Tangle) }
+            {Math.Round(Math.Cos(angle)), Math.Round(-Math.Sin(angle))},
+            {Math.Round(Math.Sin(angle)),  Math.Round(Math.Cos(angle))}
         };
-        double[,] result = new double[2, cols];
-        for (int i = 0; i < cols; i++)
-        {
-            result[0, i] = rotation[0, 0] * matrix[0, i] + rotation[0, 1] * matrix[1, i];
-            result[1, i] = rotation[1, 0] * matrix[0, i] + rotation[1, 1] * matrix[1, i];
-        }
-        return result;
+        double[,] result = new double[rows, col2];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < rotation.GetLength(1); j++)
+                {
+                    double temp = 0;
+                    for (int k = 0; k < cols; k++)
+                    {
+                        temp += matrix1[i, k] * matrix2[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            }
+            return result;
     }
 }
