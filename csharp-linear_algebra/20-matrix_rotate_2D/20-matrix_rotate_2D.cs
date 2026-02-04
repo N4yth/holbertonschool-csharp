84c406dc27136a1880c091307d478b53e5627b30
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 10:27:02 +0100

    fix

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -5,31 +5,23 @@ class MatrixMath
 {
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        int row = matrix.GetLength(0);
-        int col = matrix.GetLength(1);
-        double[,] rotation = new double[,] {{Math.Round(Math.Cos(angle)), Math.Round(-Math.Sin(angle))}, {Math.Round(Math.Sin(angle)), Math.Round(Math.Cos(angle))}};
-        int Rcol = rotation.GetLength(1);
-        if (row != 2)
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        if (rows != 2)
         {
-            double[,] result = {{-1}};
-            return result;
+            return new double[,] { { -1 } };
         }
-        else
+        double angle = angleDegrees * Math.PI / 180.0;
+        double[,] rotation = new double[,] {
+            { Math.Cos(angle), -Math.Sin(angle) },
+            { Math.Sin(angle),  Math.Cos(angle) }
+        };
+        double[,] result = new double[2, cols];
+        for (int i = 0; i < cols; i++)
         {
-            double[,] result = new double[row, col];
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < Rcol; j++)
-                {
-                    double temp = 0;
-                    for (int k = 0; k < col; k++)
-                    {
-                        temp += matrix[i, k] * rotation[k, j];
-                    }
-                    result[i, j] = temp;
-                }
-            }
-            return result;
+            result[0, i] = rotation[0, 0] * matrix[0, i] + rotation[0, 1] * matrix[1, i];
+            result[1, i] = rotation[1, 0] * matrix[0, i] + rotation[1, 1] * matrix[1, i];
         }
+        return result;
     }
 }
