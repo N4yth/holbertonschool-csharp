a809068a9b9d6439dd3e6631495a68be876d43e8
N4yth <nathan.dupuis31@gmail.com>
2026-02-04 15:38:37 +0100

    feat : create a methode that do the cross product

---
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MatrixMath
+{
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length() != 3 || vector2.Length() != 3)
+        {
+            return new double[] {{-1}};
+        }
+        return new double[] {{
+            vector1[2]*vector2[3] - vector1[3]*vector2[2],
+            vector1[3]*vector2[1] - vector1[1]*vector2[3],
+            vector1[1]*vector2[2] - vector1[2]*vector2[1]
+            }};
+    }
+}
