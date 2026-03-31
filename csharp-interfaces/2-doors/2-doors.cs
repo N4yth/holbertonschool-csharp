806813e88925e47dd1e9b9245b2ad47e7f929c36
N4yth <nathan.dupuis31@gmail.com>
2026-03-31 14:58:12 +0200

    feat: add Base class and interfaces for interactive, collectible, and breakable objects

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+abstract class Base 
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return "" + name + " is a " + GetType();
+    }
+}
+
+interface IInteractive
+{
+    public void Interact();
+}
+
+interface ICollectable
+{
+    public bool isCollected { get; set; }
+
+    public void Collect();
+}
+
+interface IBreakable
+{
+    public int durability { get; set; }
+    public void Break();
+}
+
+class Door : Base, IInteractive
+{
+    public Door (string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the " + this.name + ". It's locked.");
+    }
+}
