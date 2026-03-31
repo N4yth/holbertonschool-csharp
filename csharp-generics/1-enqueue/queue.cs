09e023a2cb02d70de9ea1a6c363ce8d325c615ca
N4yth <nathan.dupuis31@gmail.com>
2026-03-31 10:08:49 +0200

    feat: implement generic Queue class with Node structure

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+class Queue<T>
+{
+    Node head;
+    Node tail;
+    int count;
+
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public class Node
+    {
+        object value = null;
+        Node next = null;
+
+        public Node(Node value)
+        {
+            next = value;
+        }
+
+        public void Enqueue(Node value)
+        {
+            if (next == null)
+            {
+                next = value;
+            }
+            else
+            {
+                Node tmp = next;
+                while (tmp.next != null)
+                {
+                    tmp = tmp.next;
+                }
+                tmp.value = value;
+            }
+        }
+
+        public int Count()
+        {
+            int nbNode = 0;
+            Node tmp = next;
+            while (tmp.next != null)
+            {
+                tmp = tmp.next;
+                nbNode++;
+            }
+            return nbNode;
+        }
+    }
+}
