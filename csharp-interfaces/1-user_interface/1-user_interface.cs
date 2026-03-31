97128dae3cdba0ed5d65ee3e2404f0b6f392033f
N4yth <nathan.dupuis31@gmail.com>
2026-03-31 14:32:03 +0200

    feat: add initial implementation of interfaces and TestObject class

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+abstract class Base 
+{
+    public string name = "";
+
+    public override string ToString()
+    {
+        return "" + name + " is a " + GetType();
+    }
+}
+
+interface IInteractive
+{
+    void Interact()
+    {
+        
+    }
+}
+
+interface IBreakable
+{
+    public int durability { get; set; }
+    void Break()
+    {
+        
+    }
+}
+
+interface ICollectable
+{
+    public bool isCollected { get; set; }
+
+    void Collect()
+    {
+        
+    }
+}
+
+class TestObject : Base, ICollectable, IBreakable, IInteractive
+{
+    public bool isCollected { get; set; }
+    public int durability { get; set; }
+}
