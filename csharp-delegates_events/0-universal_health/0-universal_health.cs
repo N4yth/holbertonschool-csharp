e47d17708c8b0034bb221c9f9b9bd7374ff20905
N4yth <nathan.dupuis31@gmail.com>
2026-04-01 10:40:43 +0200

    feat: add Player class and project files for health management system

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default..");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.hp = this.maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+}
